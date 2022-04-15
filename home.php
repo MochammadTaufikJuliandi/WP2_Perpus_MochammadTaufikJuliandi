151da4b158aaf8708e884e300a7c867090d37187
MochammadTaufikJuliandi <taufik.juliandi4@gmail.com>
2022-04-15 23:12:13 +0700

    origin

---
+++ home.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>UBSI Cake</title>
+
+  <link rel="stylesheet" href="vendors/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.theme.default.min.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+  <link rel="stylesheet" href="vendors/Magnific-Popup/magnific-popup.css">
+
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+  <!--================ HEADER=================-->
+  <header class="header_area">
+    <div class="main_menu">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container box_1620">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+            <ul class="nav navbar-nav menu_nav justify-content-end">
+              <li class="nav-item active"><a class="nav-link" href="home.php">Home</a></li> 
+              <li class="nav-item"><a class="nav-link" href="about.php">Tentang Kami</a></li> 
+              <li class="nav-item"><a class="nav-link" href="menu.php">Menu</a>
+            </ul>
+          </div> 
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!--================ BANNER =================-->
+  <section class="hero-banner">
+    <div class="hero-wrapper">
+      <div class="hero-left">
+        <h1 class="hero-title">UBSI Cake </h1>
+        <div class="d-sm-flex flex-wrap">
+          <a class="button button-hero button-shadow" href="about.php">Pesan Sekarang!</a>
+        </div>
+        <ul class="hero-info d-none d-lg-block">
+          <li>
+            <img src="img/banner/fas-service-icon.png" alt="">
+            <h6>Pelayanan Cepat</h6>
+          </li>
+          <li>
+            <img src="img/banner/fresh-food-icon.png" alt="">
+            <h6>Rasa Tak Terkalahkan</h6>
+          </li>
+          <li>
+            <img src="img/banner/support-icon.png" alt="">
+            <h6>24/7 Siap Melayani</h6>
+          </li>
+        </ul>
+      </div>
+      <div class="hero-right">
+        <div class="owl-carousel owl-theme hero-carousel">
+          <div class="hero-carousel-item">
+            <img class="img-fluid" src="img/black_forest.jpeg" alt="">
+          </div>
+          <div class="hero-carousel-item">
+            <img class="img-fluid" src="img/onde_onde1.jpg" alt="">
+          </div>
+        </div>
+    </div>
+  </section>
+
+  <!--================TENTANG KAMI =================-->
+  <section class="about section-margin pb-xl-70">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-xl-6 mb-5 mb-md-0 pb-5 pb-md-0">
+          <div class="img-styleBox">
+            <div class="styleBox-border">
+              <img class="styleBox-img1 img-fluid" src="img/buat_kue1.jpg" alt="">
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 pl-md-5 pl-xl-0 offset-xl-1 col-xl-5">
+          <div class="section-intro mb-lg-4">
+            <h4 class="intro-title">Tentang Kami</h4>
+            <h5>“Life is a cake and love is the icing on top of it. Without love, it becomes difficult to swallow life.”</h5>
+          </div>
+          <div class="quote-wrapper">
+            <div class="quotes">
+                "Hidup adalah kue dan cinta adalah lapisan gula di atasnya. Tanpa cinta, menjadi sulit untuk menelan kehidupan."
+            </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!--================PRODUK UNGGULAN=================-->
+  <section class="section-margin mb-lg-100">
+    <div class="container">
+      <div class="section-intro mb-75px">
+        <h4 class="intro-title">Produk Unggulan Kami</h4>
+        <h2>Dibuat Sepenuh Hati()</h2>
+      </div>
+
+
+      <div class="owl-carousel owl-theme featured-carousel">
+        <div class="featured-item">
+          <img class="card-img rounded-0" src="img/klepon1.jpg" alt="">
+          <div class="item-body">
+              <h3>Klepon</h3>
+            <p>Kue klepon yang ditaburi oleh kelapa dan dengan kualitas premium siap menemani hari harimu</p>
+            <div class="d-flex justify-content-between">
+              <h3 class="price-tag">Rp. 20.000/porsi</h3>
+            </div>
+          </div>
+        </div>
+        <div class="featured-item">
+          <img class="card-img rounded-0" src="img/bika_ambon2.jpg" alt="">
+          <div class="item-body">
+              <h3>Bika Ambon</h3>
+            <p>Bika Ambon yang dibuat dengan menggunakan resep spesial yang dijamin kenikmatan rasa yang sangat enak dan tekstur yan lembut menjadikan kue ini pasti bikin nagih</p>
+            <div class="d-flex justify-content-between">
+              <h3 class="price-tag">Rp. 60.000</h3>
+            </div>
+          </div>
+        </div>
+        <div class="featured-item">
+          <img class="card-img rounded-0" src="img/black_forest3.jpg" alt="">
+          <div class="item-body">
+              <h3>Black Forest</h3>
+            <p>Lempengan Coklat yang menghiasi di lingkaran kue ini memperkaya rasa coklatnya. Bahan bahan yang kami pakaipun tidak memakai bahan pengawet</p>
+            <div class="d-flex justify-content-between">
+              <h3 class="price-tag">Rp. 120.000</h3>
+            </div>
+          </div>
+        </div>
+        <div class="featured-item">
+          <img class="card-img rounded-0" src="img/cup_cake1.jpg" alt="">
+          <div class="item-body">
+              <h3>Cup cake</h3>
+            <p>Bentuknya yang imut ditambah dengan cita rasa yang manis membuat siapa saja yang memakan nya merasakan kenikmatan yang begitu nikmat</p>
+            <div class="d-flex justify-content-between">
+              <h3 class="price-tag">Rp. 80.0000</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--================PROMO=================-->
+  <section class="bg-lightGray section-padding">
+    <div class="container">
+      <div class="row no-gutters">
+        <div class="col-sm">
+          <img class="card-img rounded-0" src="img/promo1.jpeg" alt="">
+        </div>
+        <div class="col-sm">
+          <div class="offer-card offer-card-position">
+            <h3>PROMO PSBB!</h3>
+            <h3>(PAKET SPESIAL BUAT BU TYA)</h3>
+            <h2>BELI 1 GRATIS 10:)</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--================DIRUMAH AJA =================-->  
+  <section class="cta-area text-center">  
+      <p>Selamat Sampai Tujuan Tanpa Lama Dalam Keadaan Hangat</p>
+      <p>Tinggal Tunggu dan Nikamti Bersama Keluarga Anda!!!</p>
+      <h2>#DIRUMAHAJA</h2><br><br>
+      <a href="logout.php"><h2>Logout</h2></a>
+    </div>
+  </section>
+
+  <script src="vendors/jquery/jquery-3.2.1.min.js"></script>
+  <script src="vendors/bootstrap/bootstrap.bundle.min.js"></script>
+  <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+  <script src="vendors/nice-select/jquery.nice-select.min.js"></script>
+  <script src="vendors/Magnific-Popup/jquery.magnific-popup.min.js"></script>
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>UBSI Cake - Menu</title>
+
+  <link rel="stylesheet" href="vendors/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="vendors/themify-icons/themify-icons.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.theme.default.min.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+  <link rel="stylesheet" href="vendors/Magnific-Popup/magnific-popup.css">
+
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body">
+   <!--================ Header =================-->
+   <header class="header_area">
+    <div class="main_menu">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container box_1620">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+            <ul class="nav navbar-nav menu_nav justify-content-end">
+              <li class="nav-item active"><a class="nav-link" href="home.php">Home</a></li> 
+              <li class="nav-item"><a class="nav-link" href="about.php">Tentang Kami</a></li> 
+              <li class="nav-item"><a class="nav-link" href="menu.php">Menu</a>
+            </ul>
+          </div> 
+        </div>
+      </nav>
+    </div>
+  </header>
+  
+  <!--================ MENU KUE =================-->  
+  <section class="section-margin">
+    <div class="container">
+      <div class="section-intro mb-75px">
+        <h4 class="intro-title">MENU</h4>
+        <h2>Traditional Cake</h2>
+      </div>
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="media align-items-center food-card">
+            <img class="mr-3 mr-sm-4" src="img/klepon2.jpg" alt="">
+            <div class="media-body">
+              <div class="d-flex justify-content-between food-card-title">
+                <h4>Klepon</h4>
+                <h3 class="price-tag">Rp. 20.000/porsi</h3>
+              </div>
+              <p>Kue klepon yang ditaburi oleh kelapa dan dengan kualitas premium siap menemani hari harimu</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="media align-items-center food-card">
+            <img class="mr-3 mr-sm-4" src="img/onde_onde2.jpg" alt="">
+            <div class="media-body">
+              <div class="d-flex justify-content-between food-card-title">
+                <h4>Onde Onde</h4>
+                <h3 class="price-tag">Rp. 30.000/toples</h3>
+              </div>
+              <p>Bola bola kue dengan taburan wijen dan berisi kacang hijau</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="media align-items-center food-card">
+            <img class="mr-3 mr-sm-4" src="img/kue_pisang.jpg" alt="">
+            <div class="media-body">
+              <div class="d-flex justify-content-between food-card-title">
+                <h4>Kue Pisang</h4>
+                <h3 class="price-tag">Rp. 50.000</h3>
+              </div>
+              <p>Pisang yang manis diselimuti dengan tepung kualitas tinggi cocok untuk menemani acara pesta dan acara keluarga</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="media align-items-center food-card">
+            <img class="mr-3 mr-sm-4" src="img/kue_lapis.png" alt="">
+            <div class="media-body">
+              <div class="d-flex justify-content-between food-card-title">
+                <h4>Kue Lapis</h4>
+                <h3 class="price-tag">Rp. 105.000</h3>
+              </div>
+              <p>Kue Lapis spesial yang terbuat dari bahan bahan pilihan dan tanpa pemanis buatan serta dipadukan dengan resep rahasia sehingga rasanya enak dan lezat</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="media align-items-center food-card">
+            <img class="mr-3 mr-sm-4" src="img/kue_putu.jpg" alt="">
+            <div class="media-body">
+              <div class="d-flex justify-content-between food-card-title">
+                <h4>Kue Putu</h4>
+                <h3 class="price-tag">Rp. 2.500/pcs</h3>
+              </div>
+              <p>Kangen jajanan masa kecil? tapi sekarang sudah jarang yang jual? Tenang, kami menjualnya dengan rasa yang enak dan harga terjangkau. siap menemani kalian bernostalgia dengan jajanan masa kecil</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-lg-6">
+          <div class="media align-items-center food-card">
+            <img class="mr-3 mr-sm-4" src="img/bika_ambon1.jpg" alt="">
+            <div class="media-body">
+              <div class="d-flex justify-content-between food-card-title">
+                <h4>Bika Ambon</h4>
+                <h3 class="price-tag">Rp. 60.000</h3>
+              </div>
+              <p>Rasanya yang legit berpadu dengan teksturnya yang kenyal tentu menimbulkan sensasi tersendiri saat kita menyantapnya.</p>
+            </div>
+          </div>
+        </div>
+  </section>  
+
+<!--================MENU KUE MODERN =================-->  
+<section class="section-margin">
+  <div class="container">
+    <div class="section-intro mb-75px">
+      <h2>Modern Cake</h2>
+    </div>
+    <div class="row">
+      <div class="col-lg-6">
+        <div class="media align-items-center food-card">
+          <img class="mr-3 mr-sm-4" src="img/black_forest2.jpg" alt="">
+          <div class="media-body">
+            <div class="d-flex justify-content-between food-card-title">
+              <h4>Black Forest</h4>
+              <h3 class="price-tag">Rp. 120.000</h3>
+            </div>
+            <p>Cita rasa whip cream dan sponge coklat yang menggiurkan dan sangat lembut, juga dengan taburan coklat yang pasti akan membuat anda bahagia. </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <div class="media align-items-center food-card">
+          <img class="mr-3 mr-sm-4" src="img/red_velvet.jpg" alt="">
+          <div class="media-body">
+            <div class="d-flex justify-content-between food-card-title">
+              <h4>Red Velvet</h4>
+              <h3 class="price-tag">Rp. 80.0000</h3>
+            </div>
+            <p>Dibuat dengan bahan bahan spesial. Yang sudah bisa dipastikan rasanya juara dan dengan tekstur yang lembut dimulut</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <div class="media align-items-center food-card">
+          <img class="mr-3 mr-sm-4" src="img/chesee_cake.jpg" alt="">
+          <div class="media-body">
+            <div class="d-flex justify-content-between food-card-title">
+              <h4>Cheese Cake</h4>
+              <h3 class="price-tag">Rp. 80.0000</h3>
+            </div>
+            <p>Rasa keju yang lembut dipadukan dengan kue yang terbuat dari bahan bahan spesial membuat kue ini sangat cocok sebagai hadiah untuk orang terkasih</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <div class="media align-items-center food-card">
+          <img class="mr-3 mr-sm-4" src="img/kue_tart.jpg" alt="">
+          <div class="media-body">
+            <div class="d-flex justify-content-between food-card-title">
+              <h4>Kue Tart</h4>
+              <h3 class="price-tag">Rp. 150.000</h3>
+            </div>
+            <p>Punya teman/sahabat/kekasih yang ulang tahun? Tapi gabisa bikin kue? tenang kami akan buatkan dengan cita rasa yang premium, bisa costum untuk nama juga loh </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-6">
+        <div class="media align-items-center food-card">
+          <img class="mr-3 mr-sm-4" src="img/cup_cake.jpg" alt="">
+          <div class="media-body">
+            <div class="d-flex justify-content-between food-card-title">
+              <h4>Cup Cake</h4>
+              <h3 class="price-tag">Rp. 80.000</h3>
+            </div>
+            <p>Bentuknya yang lucu dan rasanya yang manis. sangat cocok dijadikan sebagai kado untuk orang orang tersayang
+            </p>
+          </div>
+        </div>
+      </div>
+</section> 
+
+  <!--================ DIRUMAH AJA =================-->  
+  <section class="cta-area text-center">  
+  <div class="container">
+      <p>Selamat Sampai Tujuan Tanpa Lama Dalam Keadaan Hangat</p>
+      <p>Tinggal Tunggu dan Nikamti Bersama Keluarga Anda!!!</p>
+      <h2>#DIRUMAHAJA</h2><br><br>
+      <a href="logout.php"><h2>Logout</h2></a>
+    </div>
+  </section>
+  <!--================ DIRUMAH AJA =================-->  
+
+  <script src="vendors/jquery/jquery-3.2.1.min.js"></script>
+  <script src="vendors/bootstrap/bootstrap.bundle.min.js"></script>
+  <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+  <script src="vendors/nice-select/jquery.nice-select.min.js"></script>
+  <script src="vendors/Magnific-Popup/jquery.magnific-popup.min.js"></script>
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <script src="js/mail-script.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
